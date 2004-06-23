38bbbb349feef248b6e331d0f8c20e188b5b7759
parasyte <parasyte>
2004-06-23 04:00:52 +0000

    Initial revision

---
+++ C_ThreadDeath.cc
@@ -0,0 +1,128 @@
+/*!\file C_ThreadDeath.cc
+  Example class that manages threads and the detection of their exits
+  through the use of condition variables.
+*/
+
+
+#include "debug.h"
+#include "C_ThreadDeath.h"
+
+ThreadMgr::ThreadMgr()
+{
+  m_threadData = new ThreadData();
+}
+
+
+ThreadMgr::~ThreadMgr()
+{
+  //delete thread data
+  delete m_threadData;
+}
+
+int 
+ThreadMgr::cancelThread(pthread_t *tid)
+{
+  return pthread_cancel(*tid);
+}
+
+/**
+   \return SUCESS = thread ID of thread, FAIL = NULL
+
+   \param thread_func user defined function to call when thread is
+   started 
+   \param arg user defined argument (passed to thread_func)
+   
+   \todo return type should be a const!!!
+*/
+int 
+ThreadMgr::createThread(pthread_t *tid, pthread_attr_t *attr, void *(*thread_func)(void *), void *arg)
+{
+  int ret_val = 0;
+
+  func_params *params = new func_params;
+  params->thread_func=thread_func;
+  params->arguments=arg;
+  params->thisObj=this;
+
+  pthread_t tmp_tid;
+  ret_val = pthread_create(&tmp_tid, (pthread_attr_t *) attr, this->func, (void *)params);
+  *tid = tmp_tid;
+
+  //std::cout << "createThread:self="<<pthread_self()<<",tid="<<*tid<<std::endl;
+
+  if(ret_val == 0)
+    {
+      m_threadData->addThread(tid, arg);
+    }
+
+  return ret_val;
+}
+
+int
+ThreadMgr::createThread(void *(*thread_func)(void *), void *arg)
+{
+  pthread_t tid;
+  return ( createThread(&tid, NULL, thread_func, arg) );
+}
+
+
+/**
+   \return SUCCESS = return is 0 and thread_return is non NULL, FAIL =
+   return is non-zero [see pthread_join()] and thread_return is NULL
+
+   \param tid the thread id to join
+   \param thread_return a pointer to the return value of the user
+   defined thread function
+*/
+int 
+ThreadMgr::joinThread(pthread_t tid, void **thread_return)
+{
+  int ret = 0;
+
+  //ret = pthread_join(tid, thread_return);
+
+  //char *pc = ((char *)(char *)*thread_return);
+  //std::cout << "join:" << tid << "|" << pc << std::endl;
+  if( (ret = pthread_join(tid, thread_return)) == 0)
+    {
+      m_threadData->removeThread(tid);
+    }
+  
+  return ret;
+}
+
+pthread_t
+ThreadMgr::joinThread(void **thread_return)
+{
+  //wait for a thread to terminate
+
+  //join terminated thread
+
+  //return a copy of the thread id (slow)
+  return (pthread_t)0;
+}
+
+void *
+ThreadMgr::func(void *arg)
+{
+
+  /** \note user does a joinThread to remove thread from managed list
+   */
+
+  //get the object pointer
+  func_params *param = (func_params *)arg;
+  
+  //register thread with current object
+  pthread_t t = pthread_self();
+  param->thisObj->m_threadData->addThread(&t, param->arguments);
+
+  //call user's thread function
+  void *return_value = param->thread_func(param->arguments);
+
+  //std::cout << "func:" << pthread_self() << "|" << (char *)return_value << std::endl;
+
+  //do pthread_exit with user's thread func. ret. val.
+  pthread_exit((void *)return_value);
+
+  return return_value;			// we never get here!!!
+}
---
+++ debug.h
@@ -0,0 +1,125 @@
+/*! \file debug.h
+    \brief project debug macros
+    
+    These macros are here for convenience. Mostly I use this kind of
+    stuff for light debug work here and there. Please feel free to
+    steal what you need and use it as your own -parasyte.
+*/
+/* NOTICE:
+    Copyright (C) 2004  Karl N. Redman (SleepingStill.com)
+
+    This file ONLY, independent and free from any and all other files,
+    in whole or in part, is NOT covered under any license and may be
+    considered free to use, in whole or in part, by any entity in any
+    way that said entity sees fit to do so. Furthermore, it is the
+    intention of the copyright holder, Karl N. Redman, that content
+    found in this file be considered public domain information.
+
+
+    For further information contact: parasyte@sleepingstill.com
+*/
+
+/*********************** NOTE:
+This file is intended to be used on a per project basis and should be
+edited accourdingly (until it is generic enough to leave alone, I
+guess). In an automake project you can drop this file into a directory
+and have local debuging ability. 
+
+If you are using autoconf be sure to define the PRINTF preprocessor in
+config.h (configure.ac) if you want to use something other than printf
+to ouput your debug messages. 
+
+
+*/
+#ifndef _DEBUG_H_
+#define _DEBUG_H_
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#ifdef DEBUG
+#include <iostream>
+#define GOT_HERE std::cout << "GOT HERE" << std::endl;
+#else
+#include <stdio.h>
+#define GOT_HERE printf("GOT HERE\n");
+#endif
+
+/** 
+    program DEBUGING STATEMENTS. 
+    
+    These would be called in the following ways:<br>
+    dprint("this is a string");<br>
+    dprint_s1("this is also a string");<br>
+    dprint_s2("foo", "bar");<br>
+    dprint_f1("%s", "thing");<br>
+    dprint_f1("%d", 1);<br>
+    dprint_f2("%s", "%d", "stuff", 69);<br>
+    dprint_f2("%s", "%x", "a hex number", 255);<br>
+    
+    This would be the output (assuming PACKAGE is defined as
+    "run-free"):<br>
+    run-free DEBUG(this is a string)<br>
+    run-free DEBUG(this is also a string)<br>
+    run-free DEBUG(foo:bar)<br>
+    run-free DEBUG(thing)<br>
+    run-free DEBUG(1)<br>
+    run-free DEBUG(stuff:69)<br>
+    run-free DEBUG(a hex number:ff)  <br>
+*/
+///compile time option
+#ifdef DEBUG	
+#define DEBUG_MODE 1
+#else
+#define DEBUG_MODE 0
+#endif
+
+
+/** @name Debugging Message output
+ *  These macros define attributes of debug message output
+ */
+//@{ 
+
+#ifndef DEBUG_PRINTF
+/// The kind of prinf() style of function debug uses for output
+#define DEBUG_PRINTF printf
+
+#ifndef DEBUG_PRINTF_INCLUDE
+/// The support header file for the debug function
+#define DEBUG_PRINTF_INCLUDE <stdio.h>
+#include <iostream>
+#include DEBUG_PRINTF_INCLUDE
+#endif //DEBUG_PRINTF_INCLUDE
+
+#endif //DEBUG_PRINTF
+
+//check that package is defined
+#ifndef PACKAGE
+#define PACKAGE "THIS PROGRAM"
+#endif
+
+/// The package name
+#define DEBUG_PREMSG PACKAGE
+/// a submessage 
+#define DEBUG_SUBMSG "DEBUG"
+//@}
+
+/** @name Debugging statments
+ *  These macros print statements to the command line when run-free is
+ *  compiled with the --enable-debug=yes directive from configure
+ */
+//@{ 
+/// print a string in debug mode -a convenience function
+#define dprint(string) dprint_s1(string)
+/// single string printing
+#define dprint_s1(string) if(DEBUG_MODE) DEBUG_PRINTF(""DEBUG_PREMSG" "DEBUG_SUBMSG"(%s)\n", string)
+/// double string printing
+#define dprint_s2(string1, string2) if(DEBUG_MODE) DEBUG_PRINTF(""DEBUG_PREMSG" "DEBUG_SUBMSG"(%s:%s)\n", string1, string2)
+/// single format debugging from gprint
+#define dprint_f1(format, data) if(DEBUG_MODE) DEBUG_PRINTF(""DEBUG_PREMSG" "DEBUG_SUBMSG"("format")\n", data)
+/// double format debugging from gprint
+#define dprint_f2(format1, format2, data1, data2) if(DEBUG_MODE) DEBUG_PRINTF(""DEBUG_PREMSG" "DEBUG_SUBMSG"("format1":"format2")\n", data1, data2)
+//@}
+
+#endif //_DEBUG_H_
